eedaedd7e109730bd120a1288f5d5d960b5e965e
Tesfu <Tesfuhabtu21@gmail>
2025-01-24 00:03:48 -0500

    Initial commit

--- CraftKnowledge/src/Milli.java
+++ CraftKnowledge/src/Milli.java
@@ -3,9 +3,9 @@ public class Milli {
 
 	public static void main(String[] args) {
 		System.out.println("This is our java project.");
-		System.out.println("This is our java project.");
-		System.out.println("This is our java project.");
-		System.out.println("This is our java project.");
+		System.out.println("Hi this is milli");
+		System.out.println("This is another commit");
+		
 
 	}
 
