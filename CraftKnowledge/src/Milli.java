3cfbd3e2025c95af535f210c2f86bebef07b8549
Tesfu <Tesfuhabtu21@gmail>
2025-01-23 23:48:19 -0500

    Our project first commit

---
+++ CraftKnowledge/src/Milli.java
@@ -0,0 +1,12 @@
+
+public class Milli {
+
+	public static void main(String[] args) {
+		System.out.println("This is our java project.");
+		System.out.println("This is our java project.");
+		System.out.println("This is our java project.");
+		System.out.println("This is our java project.");
+
+	}
+
+}
