5bff97cbee4f1f02225cd32eb67ec47c88a2828a
Tesfu <Tesfuhabtu21@gmail>
2025-01-24 00:34:18 -0500

    adder

--- CraftKnowledge/src/Milli.java
+++ CraftKnowledge/src/Milli.java
@@ -1,11 +1,22 @@
 
 public class Milli {
+	int a = 5;
+	int b = 2;
+	
+	public void add() {
+		System.out.println(a + b);
+	}
 
 	public static void main(String[] args) {
+		
+		Milli m = new Milli();
+		
+		m.add();
+		
 		System.out.println("This is our java project.");
 		System.out.println("This is our java project.");
-		System.out.println("This is milli");
-		System.out.println("This is our java project.");
+
+
 
 	}
 	
