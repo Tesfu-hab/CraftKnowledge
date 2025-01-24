737150e9109e1a3fb7de6b371c3ae7c83727ce7a
Tesfu <Tesfuhabtu21@gmail>
2025-01-24 00:07:41 -0500

    Commit

--- CraftKnowledge/src/Milli.java
+++ CraftKnowledge/src/Milli.java
@@ -3,10 +3,11 @@ public class Milli {
 
 	public static void main(String[] args) {
 		System.out.println("This is our java project.");
-		System.out.println("Hi this is milli");
-		System.out.println("This is another commit");
-		
+		System.out.println("This is our java project.");
+		System.out.println("This is milli");
+		System.out.println("This is our java project.");
 
 	}
+	
 
 }
